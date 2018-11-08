d95c6f088b8a25764cb0453d6c2696750feb2557
怀虚 <huaixu.zy@alibaba-inc.com>
2018-11-08 14:28:43 +0800

    another try

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,7 +2,13 @@ Page({
     go: function(){
         console.log('hhhhhh')
         tt.navigateTo({
-            url: 'pages/go/index'
-        })
+            url: `pages/go/index`,
+            success (res) {
+                console.log(`success`);
+            },
+            fail (res) {
+                console.log(`navigateTo调用失败`);
+            }
+        });
       }
 })
